1bf77999f01ed2d8744b235f92cf6cf5c5d0ab58
Arjun <arjun_deshmukh_aiml@moderncoe.edu.in>
2025-11-11 19:22:27 +0530

    Update Vercel serverless function handler to properly route API requests

--- api/index.js
+++ api/index.js
@@ -1,2 +1,9 @@
-// Vercel serverless function - exports the Express app
-module.exports = require('../server.js');
+// Vercel serverless function - exports the Express app as a handler
+const app = require('../server.js');
+
+// Export as Vercel serverless function handler
+module.exports = (req, res) => {
+  // Remove /api prefix from path for Express routing
+  req.url = req.url.replace(/^\/api/, '') || '/';
+  return app(req, res);
+};
